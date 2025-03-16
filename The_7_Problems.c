3511f21c8709f07a20b13666f2cc3b70f5cc6ac5
Vinayak Gubber <gubbervinayak@gmail.com>
2025-03-16 20:38:25 +0530

    First Commit

---
+++ The_7_Problems.c
@@ -0,0 +1,66 @@
+// Maximum and minimum of an array using minimum number of comparisons
+
+// Code for Optimal Approach (Uses comparing in pairs) - The idea is that when n is odd then initialize min and max as the first element. If n is even then initialize min and max as minimum and maximum of the first two elements respectively. For the rest of the elements, pick them in pairs and compare their maximum and minimum with max and min respectively.
+#include <stdio.h>
+void getMinMax(int arr[], int n, int *min, int *max)
+{
+    int i = 0;
+
+    // Initialize min and max based on whether n is even or odd
+    if (n % 2 == 0)
+    {
+        if (arr[0] > arr[1])
+        {
+            *max = arr[0];
+            *min = arr[1];
+        }
+        else
+        {
+            *min = arr[0];
+            *max = arr[1];
+        }
+        i = 2; // Start loop from index 2
+    }
+    else
+    {
+        *min = *max = arr[0];
+        i = 1; // Start loop from index 1
+    }
+
+    // Process elements in pairs
+    while (i < n - 1)
+    {
+        if (arr[i] > arr[i + 1])
+        {
+            if (arr[i] > *max)
+                *max = arr[i];
+            if (arr[i + 1] < *min)
+                *min = arr[i + 1];
+        }
+        else
+        {
+            if (arr[i + 1] > *max)
+                *max = arr[i + 1];
+            if (arr[i] < *min)
+                *min = arr[i];
+        }
+        i += 2; // Increment by 2 as we process pairs
+    }
+}
+
+int main()
+{
+    int arr[] = {1000, 11, 445, 1, 330, 3000};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int min, max;
+
+    getMinMax(arr, n, &min, &max);
+
+    // Print results
+    printf("Minimum element is %d\n", min);
+    printf("Maximum element is %d\n", max);
+
+    return 0;
+}
+
+// End of problem
